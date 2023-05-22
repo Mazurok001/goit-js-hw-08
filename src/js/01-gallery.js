069211495c269b0cd9127ebd92f521d354382393
Mazurok001 <vasok1991@gmail.com>
2023-05-22 13:10:16 +0300

    fixed simpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 // Описаний в документації
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
@@ -12,40 +12,13 @@ const gallery = document.querySelector('.gallery');
 
 const markup = galleryItems
   .map(({ preview, original, description }) => {
-    return `<li class="gallery__item"><a class="gallery__link" href='${original}'><img class="gallery__image" src='${preview}' data-source='${original}' alt='${description}'/></a></li>`;
+    return `<li class="gallery__item"><a class="gallery__link" href='${original}'><img class="gallery__image" src='${preview}' alt='${description}'/></a></li>`;
   })
   .join('');
 
 gallery.insertAdjacentHTML('afterbegin', markup);
 
-console.log(document.querySelector('a'));
-
-gallery.addEventListener('click', handleGetImg);
-
-function handleGetImg(e) {
-  e.preventDefault();
-
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  const imgUrl = e.target.dataset.source;
-  console.log(imgUrl);
-  const modal = basicLightbox.create(
-    `<img src="${imgUrl}" width="800" height="600">`,
-    {
-      onShow: () => {
-        window.addEventListener('keydown', onEscKeyPress);
-      },
-      onClose: () => {
-        window.removeEventListener('keydown', onEscKeyPress);
-      },
-    }
-  );
-
-  modal.show();
-  function onEscKeyPress(e) {
-    if (e.code !== 'Escape') return;
-    modal.close();
-  }
-}
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
