5d2762c75e1bb542c45e6711a5c46732a64b49e1
Mazurok001 <vasok1991@gmail.com>
2023-05-25 21:40:02 +0300

    add lib throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 console.log(iframe);
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const onPlay = function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
